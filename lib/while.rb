87bba9a16738e3bf10a2041a8de7974174d3e24f
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-03 02:34:49 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
 def using_while
   levitation_force = 6
-# 	while levitation_force < 10
-# 	  levitation_force += 1
-# 	  puts "Wingardium Leviosa"
-# 	end
+	while levitation_force < 10
+	  levitation_force += 1
+	  puts "Wingardium Leviosa"
+	end
 	until levitation_force == 10
 	  levitation_force += 1
 	  puts "Wingardium Leviosa"
