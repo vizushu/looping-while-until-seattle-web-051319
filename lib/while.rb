04baa8a4ec3867917a2ebfcddbd6e35b9e98f110
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-03 02:31:07 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -5,7 +5,12 @@ def using_while
 	  puts "Wingardium Leviosa"
 	end
 	until levitation_force == 10
-	puts 
+	puts "Wingardium Leviosa"
+	levitation_force += 1
 end
-
-
+end
+# counter = 0
+# until counter == 20
+#   puts "The current number is less than 20."
+#   counter += 1
+# end
